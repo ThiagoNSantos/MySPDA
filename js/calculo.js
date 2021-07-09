94555d8f678f665ee317b0783bf437cdc3fc87db
ThiagoNSantos <82063926+ThiagoNSantos@users.noreply.github.com>
2021-07-09 16:59:59 -0300

    Organização de Arquivos

---
+++ js/calculo.js
@@ -0,0 +1,12 @@
+let el = document.getElementsByClassName('bt-estrutura');
+
+
+
+function myFunction(botao) {
+    
+    /*el.classList.remove('bt-estrutura');
+    el.classList.add('bt-estrutura-uso');*/
+
+    console.log("o botão apetado foi " + botao);
+    
+}
